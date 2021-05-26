f47b623ef815a019a4bfd5d5b08323fd6271ca8e
erickYu <ygygood@naver.com>
2021-05-26 09:44:11 +0900

    tet

--- src/main/java/com/ygygood/DevopsTest/App.java
+++ src/main/java/com/ygygood/DevopsTest/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Good!");
         System.out.println("Test!");
+        System.out.println("Test2");
     }
 }
